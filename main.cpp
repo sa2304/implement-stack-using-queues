429797c0ddfe9ff0454b3b585365853aeeef8313
Антон Шмидт <schmidt.ajax@gmail.com>
2022-02-01 21:22:21 +0700

    Solution

--- main.cpp
+++ main.cpp
@@ -1,32 +1,59 @@
 #include <cassert>
 #include <iostream>
+#include <queue>
 
 using namespace std;
 
 class MyStack {
-public:
-    MyStack() {
-
+  template <class Q>
+  void moveAllButLast(Q& src, Q& dest) {
+    while (1 < src.size()) {
+      dest.push(src.front());
+      src.pop();
     }
+  }
 
-    void push(int x) {
-      // FIXME
-    }
+ public:
+  MyStack() {
 
-    int pop() {
-      // FIXME
-      return -1;
-    }
+  }
 
-    int top() {
-      // FIXME
-      return -1;
+  void push(int x) {
+    if (data_in_q1) {
+      q1.push(x);
+    } else {
+      q2.push(x);
     }
+  }
 
-    bool empty() {
-      // FIXME
-      return false;
+  int pop() {
+    int retval;
+    if (data_in_q1) {
+      moveAllButLast(q1, q2);
+      retval = q1.back();
+      q1.pop();
+    } else {
+      moveAllButLast(q2, q1);
+      retval = q2.back();
+      q2.pop();
     }
+    data_in_q1 = not data_in_q1;
+
+    return retval;
+  }
+
+  int top() {
+    return (data_in_q1) ? q1.back() : q2.back();
+  }
+
+  bool empty() {
+    return (data_in_q1) ? q1.empty() : q2.empty();
+  }
+
+ private:
+  queue<int> q1;
+  queue<int> q2;
+  bool data_in_q1 = true;
 };
 
 void TestMyStack() {
